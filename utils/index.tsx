ff7ab418bcf3ade6638eff599ef1ebe3340a63aa
AlpayGencSoftware <gencoalpay@gmail.com>
2023-10-23 11:51:36 +0300

    Custom filter added

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,11 +1,12 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps} from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters:FilterProps) {
+    const {manufacturer, year, model, limit, fuel}=filters;
     const headers={
 		'X-RapidAPI-Key': '5d76ed1bf0msh332acca5afcdbebp13beb5jsnceb4810da61d',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-    const response =await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
+    const response =await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{
         headers:headers,
     }); 
 
@@ -38,4 +39,10 @@ export const generateCarImageUrl=(car:CarProps, angle:string)=>{
    url.searchParams.append('angle', `${angle}`);
    
    return `${url}`
+}
+
+export const updatesSearchParams=(type:string, value:string)=>{
+    const searchParams= new URLSearchParams(window.location.search);
+    searchParams.set(type, value)
+    const newPathName=`${window.location.pathname}?${searchParams.toString()}`;
 }
