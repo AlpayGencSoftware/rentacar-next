a8d4903a080a750f88bd33d99e37c77d726ad9c1
AlpayGencSoftware <gencoalpay@gmail.com>
2023-10-27 16:45:06 +0300

    All problem solved

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -10,7 +10,7 @@ const ShowMore = ({pageNumber, isNext, setLimit}:ShowMoreProps) => {
     const handleNavigation=()=>{ 
         const newLimit=(pageNumber+1)*10;
         setLimit(newLimit)
-    }
+    } 
  
   return (
     <div className="w-full flex-center gap-5 mt-10">
